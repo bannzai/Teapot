10dcfe2967a1ef2bd348f24b3e58bf7ba301171a
bannzai <kingkong999yhirose@gmail.com>
2019-04-08 23:05:04 +0900

    :sparkles: Fix definition of dependencies

--- Package.swift
+++ Package.swift
@@ -6,15 +6,15 @@ import PackageDescription
 let package = Package(
     name: "Kyusu",
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/kareman/SwiftShell.git", from: Version(4, 1, 2)),
+        .package(url: "https://github.com/kylef/PathKit.git", from: Version(0, 9, 2)),
+        .package(url: "https://github.com/behrang/YamlSwift.git", from: Version(3, 4, 0)),
+        .package(url: "https://github.com/bannzai/Ocha.git", from: Version(1, 0, 3)) 
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Kyusu",
-            dependencies: []),
+            dependencies: ["Ocha", "SwiftShell", "PathKit", "YamlSwift"]),
         .testTarget(
             name: "KyusuTests",
             dependencies: ["Kyusu"]),
