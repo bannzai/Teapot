13d1610c6bf8b85544f73d8aafb5ff1ca6765cb0
bannzai <kingkong999yhirose@gmail.com>
2019-04-08 23:11:12 +0900

    :syringe: Add Commander

--- Package.swift
+++ Package.swift
@@ -9,7 +9,8 @@ let package = Package(
         .package(url: "https://github.com/kareman/SwiftShell.git", from: Version(4, 1, 2)),
         .package(url: "https://github.com/kylef/PathKit.git", from: Version(0, 9, 2)),
         .package(url: "https://github.com/behrang/YamlSwift.git", from: Version(3, 4, 0)),
-        .package(url: "https://github.com/bannzai/Ocha.git", from: Version(1, 0, 3)) 
+        .package(url: "https://github.com/bannzai/Ocha.git", from: Version(1, 0, 3)),
+        .package(url: "https://github.com/kylef/Commander.git", from: Version(0, 8, 0))
     ],
     targets: [
         .target(
