04ed20190f7e9a6bd392eae430abb349c8ba9d79
bannzai <kingkong999yhirose@gmail.com>
2019-06-24 11:07:22 +0900

    :sparkles: Add debugger commands

--- Sources/Kyusu/main.swift
+++ Sources/Kyusu/main.swift
@@ -8,7 +8,9 @@ import Commander
 let main = command { (filePath: String) in
     print("filePath: \(filePath)")
     let config = try YamlConfigReader().read(filePath: filePath)
+    print("config: \(config)")
     let infos = ConfigurationTranslator().translate(config: config)
+    print("infos: \(infos)")
     try infos.forEach { try TeapotCommandExecutor().exec(information: $0) }
 }
 
