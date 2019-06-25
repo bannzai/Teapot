c2976a921b94f6833579e8dcbd0065de8d585c36
bannzai <kingkong999yhirose@gmail.com>
2019-06-26 08:24:29 +0900

    :up: swift-tool-version

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:4.2
+// swift-tools-version:5.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -9,7 +9,7 @@ let package = Package(
         .package(url: "https://github.com/kareman/SwiftShell.git", from: Version(4, 1, 2)),
         .package(url: "https://github.com/kylef/PathKit.git", from: Version(0, 9, 2)),
         .package(url: "https://github.com/behrang/YamlSwift.git", from: Version(3, 4, 0)),
-        .package(url: "https://github.com/bannzai/Ocha.git", from: Version(1, 0, 3)),
+        .package(url: "https://github.com/bannzai/Ocha.git", from: Version(1, 1, 0)),
         .package(url: "https://github.com/kylef/Commander.git", from: Version(0, 8, 0))
     ],
     targets: [
