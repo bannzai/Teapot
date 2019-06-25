2da71b2661beb9b5c7cf3bcd3fca29d065261b7a
bannzai <kingkong999yhirose@gmail.com>
2019-06-25 10:05:43 +0900

    :recycle: Fix glob structure

--- Sources/Kyusu/Reader/Glob.swift
+++ Sources/Kyusu/Reader/Glob.swift
@@ -8,34 +8,33 @@
 import Foundation
 import Darwin
 
-public func glob(basePath: Path, pattern: String) -> [Path] {
-    let globURL: Path
-    switch basePath.last == "/" {
-    case true:
-        globURL = basePath + pattern
-    case false:
-        globURL = basePath + "/" + pattern
-    }
+public struct Glob {
+    private static let globFlags = GLOB_TILDE | GLOB_BRACE | GLOB_MARK
     
-    var gt = glob_t()
-    let cPattern = strdup(globURL)
-    defer {
-        globfree(&gt)
-        free(cPattern)
-    }
-    
-    let flags = GLOB_TILDE | GLOB_BRACE | GLOB_MARK
-    if Darwin.glob(cPattern, flags, nil, &gt) == 0 {
-        let matchc = gt.gl_matchc
-        return (0..<Int(matchc)).compactMap { index in
-            if let path = String(validatingUTF8: gt.gl_pathv[index]!) {
-                return path
-            }
-            
-            return nil
+    public static func glob(basePath: Path, pattern: String) -> [Path] {
+        let globURL: Path
+        switch basePath.last == "/" {
+        case true:
+            globURL = basePath + pattern
+        case false:
+            globURL = basePath + "/" + pattern
         }
+        
+        var gt = glob_t()
+        let cPattern = strdup(globURL)
+        defer {
+            globfree(&gt)
+            free(cPattern)
+        }
+        
+        if Darwin.glob(cPattern, Glob.globFlags, nil, &gt) == 0 {
+            let matchc = gt.gl_matchc
+            return (0..<Int(matchc)).compactMap { index in
+                String(validatingUTF8: gt.gl_pathv[index]!)
+            }
+        }
+        
+        // GLOB_NOMATCH
+        return []
     }
-    
-    // GLOB_NOMATCH
-    return []
 }
