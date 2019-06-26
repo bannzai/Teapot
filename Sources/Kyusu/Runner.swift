99013836ad026d5cc12be5200280eddc145d5825
bannzai <kingkong999yhirose@gmail.com>
2019-06-26 15:32:50 +0900

    :recycle: Using dependency

--- Sources/Kyusu/Runner.swift
+++ Sources/Kyusu/Runner.swift
@@ -12,12 +12,14 @@ import Ocha
 import PathKit
 import Commander
 
+fileprivate let errorLogPrefix = "[ERROR]🚫: "
 public struct Runner<T: Translator>
     where T.Input == Config, T.Output == [ExecutorInformation] {
+
     public struct Dependency {
-        private let configurationTranslator: T
-        private let configReader: ConfigReader
-        private let executor: Executor
+        let configurationTranslator: T
+        let configReader: ConfigReader
+        let executor: Executor
         
         public init(
             configurationTranslator: T,
@@ -32,7 +34,7 @@ public struct Runner<T: Translator>
 
     private let workingDirectory: String
     private let dependency: Dependency
-    
+
     public init(
         workingDirectory: String,
         dependency: Dependency
@@ -40,34 +42,46 @@ public struct Runner<T: Translator>
         self.workingDirectory = workingDirectory
         self.dependency = dependency
     }
+    
+    public typealias DefaultTranslator = ConfigurationTranslator<FilePathExtractor, FilePathCollector>
+    public static func create() -> Runner<DefaultTranslator> {
+        let currentWorkingDirectory: String
+        switch ProcessInfo.processInfo.environment["DEBUG_PWD"] {
+        case nil:
+            currentWorkingDirectory = Process().currentDirectoryPath
+        case let pwd?:
+            currentWorkingDirectory = pwd
+        }
+        return Runner<DefaultTranslator>(
+            workingDirectory: currentWorkingDirectory,
+            dependency: Runner<DefaultTranslator>.Dependency(
+                configurationTranslator: DefaultTranslator(
+                    extractor: FilePathExtractor(),
+                    sourcePathCollector: FilePathCollector(baseFilePath: currentWorkingDirectory, accessor: \.sourcePaths),
+                    ignorePathCollector: FilePathCollector(baseFilePath: currentWorkingDirectory, accessor: \.ignoredPaths)
+                ),
+                configReader: YamlConfigReader(),
+                executor: TeapotCommandExecutor()
+            )
+        )
+    }
 
     public func run() {
-        if let pwd = ProcessInfo.processInfo.environment["DEBUG_PWD"] {
-            SwiftShell.main.currentdirectory = pwd
-        }
-        let errorLogPrefix = "[ERROR]🚫: "
-        let currentWorkingDirectory = SwiftShell.main.currentdirectory
-        
+
         let config: Config
         do {
-            config = try YamlConfigReader().read(filePath: teapotYamlFileName)
+            config = try dependency.configReader.read(filePath: teapotYamlFileName)
         } catch {
             print(errorLogPrefix + "Can not read yaml file. " + error.localizedDescription)
             exit(1)
         }
         
-        let infos = ConfigurationTranslator(
-            extractor: FilePathExtractor(),
-            sourcePathCollector: FilePathCollector(baseFilePath: currentWorkingDirectory, accessor: \.sourcePaths),
-            ignorePathCollector: FilePathCollector(baseFilePath: currentWorkingDirectory, accessor: \.ignoredPaths)
-            )
-            .translate(config: config)
-        
+        let infos = dependency.configurationTranslator.translate(config: config)
         let watcher = Ocha.Watcher(paths: infos.map { $0.path })
         watcher.start { (events) in
             // FIXME: Watcher can not register throws keyword. Because Watcher.start call from Objective-C API.
             do {
-                try infos.forEach { try TeapotCommandExecutor().exec(information: $0) }
+                try infos.forEach { try self.dependency.executor.exec(information: $0) }
             } catch {
                 print(errorLogPrefix + error.localizedDescription)
             }
