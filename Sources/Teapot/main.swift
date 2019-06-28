c8952f789f89da887613f50d15b38b68ae37f73e
bannzai <kingkong999yhirose@gmail.com>
2019-06-28 13:45:58 +0900

    :sparkles: Add setup command

--- Sources/Teapot/main.swift
+++ Sources/Teapot/main.swift
@@ -1,10 +1,16 @@
 import Foundation
 import TeapotCore
+import Commander
 
-func run() {
-    let runner = Runner<Runner.DefaultTranslator>.create()
-    runner.run()
-    RunLoop.main.run()
+Group {
+    $0.command("setup", {
+        print("setup")
+    })
+    $0.command("start", {
+        let runner = Runner<Runner.DefaultTranslator>.create()
+        runner.run()
+        RunLoop.main.run()
+    })
 }
+.run()
 
-run()
