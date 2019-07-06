d1b98fc887423d6e53e5286954d76e9ed1b9f7bb
bannzai <kingkong999yhirose@gmail.com>
2019-07-06 17:45:15 +0900

    :recycle: Remove tests

--- development/scripts/generate/auto_mockable.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash 
-set -eu
-set -o pipefail
-
-PWD=`dirname $0`
-APP_DIR="$PWD/../../../"
-
-cd $APP_DIR
-sourcery --sources ./Sources/TeapotCore/ --templates ./development/templates/sourcery/AutoMockable.stencil --output ./Tests/TeapotCoreTests/Mock.generated.swift 
